7fbea18ced9ebb090bc3b136d0a3e9ea90759abc
EdagPSIT <134361096+EdagPSIT@users.noreply.github.com>
2024-01-17 20:45:51 +0530

    Update common_transformation.py

--- src/machinefailure/features/common_transformation.py
+++ src/machinefailure/features/common_transformation.py
@@ -4,11 +4,18 @@ import numpy as np
 
 # Log transformer class to tranform the numerical variables
 class LogTransformer(BaseEstimator, TransformerMixin):
+    ""Logarithmic transformer for numerical variables."""
+    
     def __init__(self):
+        """Initialize the LogTransformer."""
         pass
     
     def fit(self, X, y=None):
+        """Fit the LogTransformer."""
         return self
     
     def transform(self, X):
-        return np.log1p(X) 
+        """Transform the input data using logarithmic transformation."""
+        if np.any(X <= 0):
+            raise ValueError("Input array must contain only positive values for log transformation.")
+        return np.log1p(X)
